2c2dd25f66215f5b5b78b8e60f31962de8347b97
kimiakalantari <kimiakalantari97@gmail.com>
2023-03-26 12:01:23 -0400

    added index.js and refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,29 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${args}`;
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(`Recieved this error from the API:  ${error}`);
-    return;
-  }
-  //console.log(typeof body)
-  const data = JSON.parse(body);
-  //console.log(data);
-  //console.log(typeof data);
-  if (data[0]) {
-    for (const key in data[0]) {
-      if (key === 'description') {
-        console.log(data[0][key]);
-      }
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(`Recieved this error from the API:  ${error}`, null);
     }
-  } else {
-    console.log(`Error: ${args} not found`);
-  }
-});
+    //console.log(typeof body)
+    const data = JSON.parse(body);
+    //console.log(data);
+    //console.log(typeof data);
+    if (data[0]) {
+      for (const key in data[0]) {
+        if (key === 'description') {
+          callback(null, data[0][key]);
+        }
+      }
+    } else {
+      callback(`Error: ${breedName} not found`, null);
+    }
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
